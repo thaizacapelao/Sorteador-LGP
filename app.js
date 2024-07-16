ad9e6bd6f70cc78b369d2ce82cad51007830275e
Thaiza Capelão <thaizacapelao@gmail.com>
2024-07-16 17:59:42 -0400

    att

---
+++ app.js
@@ -0,0 +1,5 @@
+function sortear() {
+    let quantidade = document.querySelectorById('quantidade').value;
+    let de = document.querySelectorById('de').value;
+    let ate = document.querySelectorById('ate').value;
+}
