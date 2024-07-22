eaa6ac471e22f44bfae3d596d412109ce8ac6eaf
Thaiza Capelão <thaizacapelao@gmail.com>
2024-07-22 17:22:10 -0400

    alterando botao de reiniciar e adicionando funcao

--- app.js
+++ app.js
@@ -14,17 +14,34 @@ let numero;
         while(sorteados.includes(numero)) {
             numero = obterNumeroAleatorio(de, ate); 
         }
-        
+
         sorteados.push(numero);
     }
 
  let resultado = document.getElementById('resultado');
  let label = resultado.querySelector('label');
  label.textContent = `Números sorteados: ${sorteados}`;
-
+ alterarStatusBotao();   
 }
 
 function obterNumeroAleatorio(min, max){
     return Math.floor(Math.random() * (max - min +1)) + min;
 }
 
+function alterarStatusBotao(){
+    let botao = document.getElementById('btn-reiniciar');
+        if(botao.classList.contains('container__botao-desabilitado')) {
+            botao.classList.remove('container__botao-desabilitado');
+            botao.classList.add('container__botao');
+        }else {
+            botao.classList.remove('container__botao');
+            botao.classList.add('container__botao-desabilitado');
+        }
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+}
